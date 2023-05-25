6f195458a566bf4cafbcc14f9df0485dba70ef12
Yuri Nogueira <personal@yurinogueira.dev.br>
2023-05-25 20:06:38 -0300

    #38 test(GameServiceUnitTest.java) coverage getGameStatus

--- backend/src/test/java/br/com/eterniaserver/xadrez/domain/service/GameServiceUnitTest.java
+++ backend/src/test/java/br/com/eterniaserver/xadrez/domain/service/GameServiceUnitTest.java
@@ -14,13 +14,15 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Stream;
 
 @ExtendWith(MockitoExtension.class)
 class GameServiceUnitTest {
@@ -28,13 +30,13 @@ class GameServiceUnitTest {
     private GameService gameService;
     private final UUID whitePlayerUUID = UUID.randomUUID();
     private final UUID blackPlayerUUID = UUID.randomUUID();
+    @Mock
     private List<MoveDto> whiteMovesMock;
+    @Mock
     private List<MoveDto> blackMovesMock;
 
     @BeforeEach
     void init() {
-        whiteMovesMock = new ArrayList<>();
-        blackMovesMock = new ArrayList<>();
         gameService = new GameService() {
             @Override
             public List<Game> getAllGames() {
@@ -73,9 +75,9 @@ class GameServiceUnitTest {
             @Override
             public List<MoveDto> getPossibleMoves(GameDto game, PieceDto piece, UUID playerUUID) {
                 if (whitePlayerUUID.equals(playerUUID)) {
-                    return whiteMovesMock;
+                    return whiteMovesMock.stream().toList();
                 }
-                return blackMovesMock;
+                return blackMovesMock.stream().toList();
             }
 
             @Override
@@ -97,6 +99,16 @@ class GameServiceUnitTest {
 
         Assertions.assertTrue(pieceDtoOptional.isPresent());
         Assertions.assertEquals(pieceDto, pieceDtoOptional.get());
+    }
+
+    @Test
+    void getOptionalPieceDtoEmpty() {
+        PieceDto pieceDto = PieceDto.builder().pieceType(PieceType.PAWN).build();
+        List<PieceDto> pieceDtoList = List.of(pieceDto);
+
+        Optional<PieceDto> pieceDtoOptional = gameService.getPieceOptional(pieceDtoList, PieceType.KING);
+
+        Assertions.assertTrue(pieceDtoOptional.isEmpty());
     }
 
     @Test
@@ -192,8 +204,7 @@ class GameServiceUnitTest {
                         .build()
                 )
                 .build();
-        whiteMovesMock.add(pawnCaptureBlackKing);
-        whiteMovesMock.add(pawnMove);
+        Mockito.when(whiteMovesMock.stream()).then(i -> Stream.of(pawnCaptureBlackKing, pawnMove));
 
         GameStatus expectStatus = GameStatus.WHITE_CHECK;
         GameStatus actualStatus = gameService.isCheck(gameDto, whiteKing, blackKing);
@@ -250,8 +261,7 @@ class GameServiceUnitTest {
                         .build()
                 )
                 .build();
-        blackMovesMock.add(pawnCaptureWhiteKing);
-        blackMovesMock.add(pawnMove);
+        Mockito.when(blackMovesMock.stream()).then(i -> Stream.of(pawnCaptureWhiteKing, pawnMove));
 
         GameStatus expectStatus = GameStatus.BLACK_CHECK;
         GameStatus actualStatus = gameService.isCheck(gameDto, whiteKing, blackKing);
@@ -333,7 +343,7 @@ class GameServiceUnitTest {
                         .build()
                 )
                 .build();
-        whiteMovesMock.add(pawnCaptureBlackKing);
+        Mockito.when(whiteMovesMock.stream()).then(i -> Stream.of(pawnCaptureBlackKing));
 
         GameStatus expectedStatus = GameStatus.WHITE_WINS;
         GameStatus actualStatus = gameService.getGameStatus(gameDto, true);
@@ -382,7 +392,7 @@ class GameServiceUnitTest {
                         .build()
                 )
                 .build();
-        blackMovesMock.add(pawnCaptureWhiteKing);
+        Mockito.when(blackMovesMock.stream()).then(i -> Stream.of(pawnCaptureWhiteKing));
 
         GameStatus expectedStatus = GameStatus.BLACK_WINS;
         GameStatus actualStatus = gameService.getGameStatus(gameDto, false);
@@ -390,5 +400,155 @@ class GameServiceUnitTest {
         Assertions.assertEquals(expectedStatus, actualStatus);
     }
 
+    @Test
+    void getGameStatusShouldReturnNormal() {
+        PieceDto whiteKing = PieceDto.builder()
+                .pieceType(PieceType.KING)
+                .positionX(6)
+                .positionY(6)
+                .whitePiece(true)
+                .build();
+        PieceDto blackKing = PieceDto.builder()
+                .pieceType(PieceType.KING)
+                .positionX(2)
+                .positionY(5)
+                .whitePiece(false)
+                .build();
+        BoardDto boardDto = BoardDto.builder()
+                .whitePieces(List.of(whiteKing))
+                .blackPieces(List.of(blackKing))
+                .histories(List.of())
+                .pieceMatrix(new Integer[8][8][2])
+                .build();
+        GameDto gameDto = GameDto.builder()
+                .board(boardDto)
+                .whitePlayerUUID(whitePlayerUUID)
+                .blackPlayerUUID(blackPlayerUUID)
+                .whiteTurn(true)
+                .build();
+
+        GameStatus expectedStatus = GameStatus.NORMAL;
+        GameStatus actualStatus = gameService.getGameStatus(gameDto, true);
+
+        Assertions.assertEquals(expectedStatus, actualStatus);
+    }
+
+    @Test
+    void getGameStatusIsWhiteCheck() {
+        PieceDto whiteKing = PieceDto.builder()
+                .pieceType(PieceType.KING)
+                .positionX(6)
+                .positionY(6)
+                .whitePiece(true)
+                .build();
+        PieceDto whitePawn = PieceDto.builder()
+                .pieceType(PieceType.PAWN)
+                .positionX(3)
+                .positionY(4)
+                .whitePiece(true)
+                .build();
+        PieceDto blackKing = PieceDto.builder()
+                .pieceType(PieceType.KING)
+                .positionX(2)
+                .positionY(5)
+                .whitePiece(false)
+                .build();
+        BoardDto boardDto = BoardDto.builder()
+                .whitePieces(List.of(whiteKing, whitePawn))
+                .blackPieces(List.of(blackKing))
+                .histories(List.of())
+                .pieceMatrix(new Integer[8][8][2])
+                .build();
+        GameDto gameDto = GameDto.builder()
+                .board(boardDto)
+                .whiteTurn(true)
+                .whitePlayerUUID(whitePlayerUUID)
+                .blackPlayerUUID(blackPlayerUUID)
+                .build();
+
+        MoveDto pawnCaptureBlackKing = MoveDto.builder()
+                .first(MoveType.CAPTURE)
+                .second(PositionDto.builder()
+                        .first(2)
+                        .second(5)
+                        .build()
+                )
+                .build();
+        MoveDto blackKingMove = MoveDto.builder()
+                .first(MoveType.NORMAL)
+                .second(PositionDto.builder()
+                        .first(2)
+                        .second(4)
+                        .build()
+                )
+                .build();
+
+        Mockito.when(whiteMovesMock.stream()).then(i -> Stream.of(pawnCaptureBlackKing)).then(i -> Stream.of());
+        Mockito.when(blackMovesMock.stream()).then(i -> Stream.of()).then(i -> Stream.of(blackKingMove));
+
+
+        GameStatus expectedStatus = GameStatus.WHITE_CHECK;
+        GameStatus actualStatus = gameService.getGameStatus(gameDto, false);
+
+        Assertions.assertEquals(expectedStatus, actualStatus);
+    }
+
+    @Test
+    void getGameStatusIsBlackCheck() {
+        PieceDto whiteKing = PieceDto.builder()
+                .pieceType(PieceType.KING)
+                .positionX(6)
+                .positionY(6)
+                .whitePiece(true)
+                .build();
+        PieceDto blackPawn = PieceDto.builder()
+                .pieceType(PieceType.PAWN)
+                .positionX(5)
+                .positionY(5)
+                .whitePiece(false)
+                .build();
+        PieceDto blackKing = PieceDto.builder()
+                .pieceType(PieceType.KING)
+                .positionX(2)
+                .positionY(5)
+                .whitePiece(false)
+                .build();
+        BoardDto boardDto = BoardDto.builder()
+                .whitePieces(List.of(whiteKing))
+                .blackPieces(List.of(blackKing, blackPawn))
+                .histories(List.of())
+                .pieceMatrix(new Integer[8][8][2])
+                .build();
+        GameDto gameDto = GameDto.builder()
+                .board(boardDto)
+                .whitePlayerUUID(whitePlayerUUID)
+                .blackPlayerUUID(blackPlayerUUID)
+                .whiteTurn(true)
+                .build();
+
+        MoveDto pawnCaptureWhiteKing = MoveDto.builder()
+                .first(MoveType.CAPTURE)
+                .second(PositionDto.builder()
+                        .first(6)
+                        .second(6)
+                        .build()
+                )
+                .build();
+        MoveDto whiteKingMove = MoveDto.builder()
+                .first(MoveType.NORMAL)
+                .second(PositionDto.builder()
+                        .first(6)
+                        .second(5)
+                        .build()
+                )
+                .build();
+        Mockito.when(blackMovesMock.stream()).then(i -> Stream.of(pawnCaptureWhiteKing)).then(i -> Stream.of());
+        Mockito.when(whiteMovesMock.stream()).then(i -> Stream.of(whiteKingMove)).then(i -> Stream.of());
+
+        GameStatus expectedStatus = GameStatus.BLACK_CHECK;
+        GameStatus actualStatus = gameService.getGameStatus(gameDto, true);
+
+        Assertions.assertEquals(expectedStatus, actualStatus);
+    }
 
 }
